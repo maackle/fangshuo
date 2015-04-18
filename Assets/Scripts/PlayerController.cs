6580030ed8c0c62eec1cd1c78145b1a25d24c7e2
Michael Dougherty <maackle.d@gmail.com>
2015-04-18 16:11:04 -0700

    turn with the force

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
     public float speed;
+    public float turnDampening = 0.1f;
+
+    private Vector2 targetDirection;
+    private float forwardAngle = -90f;
 
 	// Update is called once per frame
 	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
+        Transform transform = GetComponent<Transform>();
 		Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
         Vector2 force = new Vector2(moveHorizontal * speed, moveVertical * speed);
         rigidbody.AddForce(force);
+        if (force != Vector2.zero) {
+	        float angle = Mathf.Atan2(force.y, force.x) * 180 / Mathf.PI + forwardAngle;
+	        Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnDampening);
+        }
 	}
 }
