256d23032c6223d429324925d55a30989cb67716
Michael Dougherty <maackle.d@gmail.com>
2015-04-18 17:56:39 -0700

    sound on collide

---
+++ Assets/Scripts/FurnitureController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class FurnitureController : MonoBehaviour {
+
+	public AudioClip bumpSound;
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		GetComponent<AudioSource>().PlayOneShot(bumpSound, 1.0f);
+	}
+}
